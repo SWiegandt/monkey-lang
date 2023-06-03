50e0c5808393391dd6810d3dfdc322a008c698d3
Sebastian Wiegandt <sebastian.wiegandt@gmail.com>
2023-06-03 16:33:25 +0200

    Add REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,20 @@
 module Main where
 
-import Control.Monad.State (evalState)
 import Lexer
+import System.Environment (getEnv)
+import System.IO (hFlush, stdout)
+import Text.Printf (printf)
+
+repl :: IO ()
+repl = do
+    putStr "> " >> hFlush stdout
+    line <- getLine
+    mapM_ print . filter (\t -> ttype t /= EOF) $ runLexer line
+    repl
 
 main :: IO ()
 main = do
-    let str = ";=)"
-    print $ runLexer str
+    user <- getEnv "USER"
+    printf "Hello %s! This is the Monkey programming language!\n" user
+    putStrLn "Feel free to type in commands"
+    repl
